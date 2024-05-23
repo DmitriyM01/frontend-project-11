3e97c5f2babb2b2c8ab4ae01b737e18d557e78b1
Dimitro <mironowd697@gmail.com>
2024-05-24 00:45:55 +0300

    fix validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import * as yup from 'yup';
 import { string } from 'yup';
 import './index.scss';
 
@@ -10,27 +9,31 @@ const schema = string().url().required();
 
 const state = {
     previousValue: '',
-    error: '',
+    error: ''
 };
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    state.error = '';
 
     const data = new FormData(form);
-    const inputValue = data.get('url');
+    const inputValue = data.get('url').trim();
 
-    if (inputValue === state.previousValue) state.textFeedBox = 'Такой RSS уже есть';
+    if (inputValue === state.previousValue) state.error = 'Такой RSS уже есть';
 
-    if (!schema.isValidSync(inputValue)) state.textFeedBox = 'Ссылка должна быть валидным URL';
+    if (!schema.isValidSync(inputValue)) state.error = 'Ссылка должна быть валидным URL';
 
     if (state.error.length > 0) {
         feedbackBox.classList.remove('text-succes');
         feedbackBox.classList.add('text-danger');
         feedbackBox.textContent = state.error;
+    } else {
+        feedbackBox.classList.remove('text-danger');
+        feedbackBox.classList.add('text-success');
+        feedbackBox.textContent = 'RSS успешно загружен';
     }
-    feedbackBox.textContent = 'RSS успешно загружен';
     textInput.value = '';
     textInput.focus();
+    console.log(state);
     state.previousValue = inputValue;
 });
