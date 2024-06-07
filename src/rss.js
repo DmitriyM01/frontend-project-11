b8014263c2c872baf03d24115b6668cbee1f1590
Dimitro <mironowd697@gmail.com>
2024-06-07 15:39:08 +0300

    qfix

--- src/rss.js
+++ src/rss.js
@@ -131,7 +131,7 @@ export default (url, watchedState) => {
         .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
         .then((response) => {
             console.log(response);
-            if (response.data.status.http_code === 200) {
+            if (response.status === 200) {
                 return parseRSS(response.data.contents, watchedState);
             }
             throw new Error(404);
